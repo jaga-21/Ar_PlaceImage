ee71fc39c39f5ff27fb87c3acaa3c1d35f47a089
Jagadeep-21 <jagasparkz3456@gmail.com>
2023-08-21 19:07:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ar_compose/ui/theme/Product.kt
@@ -0,0 +1,7 @@
+package com.example.ar_compose.ui.theme
+data class Product(
+    val id: Int,
+    val name: String,
+    val description: String,
+    // Add more properties like price, images, etc.
+)
